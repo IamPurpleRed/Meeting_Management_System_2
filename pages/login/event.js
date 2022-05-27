4152bded6e9e2679632c5d459b620568c0637295
Dandick <danny040414@gmail.com>
2022-05-27 23:54:03 +0800

    5_27 動態寬

--- pages/login/event.js
+++ pages/login/event.js
@@ -1,30 +1,28 @@
 const user1=document.getElementById('user1');
-const userLeft=document.getElementById('userLeft');
-const userRight=document.getElementById('userRight');
 var count=0;
 had=false;
 user1.addEventListener("click",showInput);
-userLeft.addEventListener("click",showInput);
-userRight.addEventListener("click",showInput);
-
-
-
-
-function showInput(){
-const newInput=document.createElement("input");
-newInput.type="text";
-newInput.size=27;
-newInput.style="font-size:52px; margin-top:150px;"
-
-const newInput2=document.createElement("input");
-newInput2.type="text";
-newInput2.size=27;
-newInput2.style="font-size:52px; margin-top:150px;"
-if(!had)
+function showInput()
 {
-    document.getElementById('test').appendChild(newInput);
-    document.getElementById('test').appendChild(newInput2);
-    had=true;
-}
+  const newInput=document.createElement("input");
+  newInput.type="text";
+  newInput.size=26;
+  newInput.style="font-size:52px; margin-top:150px;background-color: rgba(256, 256, 256, 0.5);"
+
+  const newInput2=document.createElement("input");
+  newInput2.type="text";
+  newInput2.size=26;
+  newInput2.style="font-size:52px; margin-top:100px;background-color: rgba(256, 256, 256, 0.5);"
+
+  const newButton=document.createElement("button");
+  newButton.style="font-size:52px; width:25%;height:8%;border-radius:20%;margin:80px auto; text-align:center; display:block;background-color:light-gray;opacity:0.7;";
+  newButton.innerText="Login";
+  if(!had)
+  {
+      document.getElementById('test').appendChild(newInput);
+      document.getElementById('test').appendChild(newInput2);
+        document.getElementById('test').appendChild(newButton);
+      had=true;
+  }
 
 }
