d809f11ee4aeb3fe2c0cdfa92cc1d3614597bca6
PurpleRed <benson060211@gmail.com>
2022-05-29 13:50:25 +0800

    搜尋頁面完成

---
+++ src/js/search.js
@@ -0,0 +1,19 @@
+$('#user_group_toggle').click(() => {
+    if ($('#user_group_toggle').text() == 'keyboard_arrow_down') {
+        $('#user_group_items').slideUp();
+        $('#user_group_toggle').text('keyboard_arrow_up');
+    } else {
+        $('#user_group_toggle').text('keyboard_arrow_down');
+        $('#user_group_items').slideDown();
+    }
+});
+
+$('#meeting_group_toggle').click(() => {
+    if ($('#meeting_group_toggle').text() == 'keyboard_arrow_down') {
+        $('#meeting_group_toggle').text('keyboard_arrow_up');
+        $('#meeting_group_items').slideUp();
+    } else {
+        $('#meeting_group_toggle').text('keyboard_arrow_down');
+        $('#meeting_group_items').slideDown();
+    }
+});
