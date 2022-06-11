aaa8be7f493ceeca80e016aaaf498f1fafa4372d
PurpleRed <benson060211@gmail.com>
2022-06-12 01:23:35 +0800

    profile UI 微調

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,10 +1,11 @@
 $(document).ready(() => {
+    setInterval(hideBtnText, 100);
+
     $('#edit_btn').click(() => {
         $('.input').removeAttr('disabled');
         $('#edit_btn').prop('disabled', true);
     });
 
-    $('#edit_btn > .text').hide();
     $('#edit_btn').hover(() => {
         $('#edit_btn').width(100);
         $('#edit_btn > .text').delay(500).fadeIn();
@@ -13,7 +14,6 @@ $(document).ready(() => {
         $('#edit_btn').width(50);
     });
 
-    $('#save_btn > .text').hide();
     $('#save_btn').hover(() => {
         $('#save_btn').width(100);
         $('#save_btn > .text').delay(500).fadeIn();
@@ -22,7 +22,6 @@ $(document).ready(() => {
         $('#save_btn').width(50);
     });
 
-    $('#photo_area > .btn > .text').hide();
     $('#photo_area > .btn').hover(() => {
         $('#photo_area > .btn').width(180);
         $('#photo_area > .btn > .text').delay(500).fadeIn();
@@ -31,3 +30,15 @@ $(document).ready(() => {
         $('#photo_area > .btn').width(50);
     });
 });
+
+function hideBtnText() {
+    if ($('#edit_btn').width() < 80) {
+        $('#edit_btn > .text').hide();
+    }
+    if ($('#save_btn').width() < 80) {
+        $('#save_btn > .text').hide();
+    }
+    if ($('#photo_area > .btn').width() < 160) {
+        $('#photo_area > .btn > .text').hide();
+    }
+}
