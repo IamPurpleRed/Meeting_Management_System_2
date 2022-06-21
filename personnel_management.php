721cc14c5074a7ba690dca6361d6dca72308a4aa
PeiWenFu <a1077160@mail.nuk.edu.tw>
2022-06-22 00:15:43 +0800

    解決profile+side_menu重疊問題

--- personnel_management.php
+++ personnel_management.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="src/css/style.css">
   <link rel="stylesheet" href="src/css/side_menu.css">
   <link rel="stylesheet" href="src/css/profile.css">
+  <link rel="stylesheet" href="src/css/personnel_overview.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- src/js/personnel_management.js
+++ src/js/personnel_management.js
@@ -1,37 +1,3 @@
-$(document).ready(() => {
-    setInterval(hideBtnText, 100);
-
-    $('#edit_btn').hover(() => {
-        $('#edit_btn').width(150);
-        $('#edit_btn > .text').delay(500).fadeIn();
-    }, () => {
-        $('#edit_btn > .text').hide();
-        $('#edit_btn').width(50);
-    });
-
-    $('#reset_pwd_btn').hover(() => {
-        $('#reset_pwd_btn').width(150);
-        $('#reset_pwd_btn > .text').delay(500).fadeIn();
-    }, () => {
-        $('#reset_pwd_btn > .text').hide();
-        $('#reset_pwd_btn').width(50);
-    });
-
-    $('#reset_pwd_btn').click(() => {
-        confirm('即將把該用戶密碼重設為123，確定要繼續？');
-    });
-});
-
-function hideBtnText() {
-    if ($('#edit_btn').width() < 130) {
-        $('#edit_btn > .text').hide();
-    }
-    if ($('#reset_pwd_btn').width() < 130) {
-        $('#reset_pwd_btn > .text').hide();
-    }
-}
-
-
 var clicking = null;
 
 function makeActive(which) {
