6faffb7e6566176596a7b4a5c1e3f52d40cd3daa
PurpleRed <benson060211@gmail.com>
2022-06-27 13:25:25 +0800

    前端樣式微調

--- components/homepage/has_meeting_banner.php
+++ components/homepage/has_meeting_banner.php
@@ -2,11 +2,11 @@
 <div id="banner">
   <div id="text_area">
     <h2 id="has_meeting_title">即將開始</h2>
-    <h2 id="meeting_name">110學年度第2學期第一次系務會議</h2>
-    <h3 id="detailed_content">時間：18:00~20:00<br>地點：工學院 200 教室</h3>
+    <h2 id="meeting_name">國立高雄大學109學年度第二學期第一次系務會議議程</h2>
+    <h3 id="detailed_content">時間：18:00<br>地點：本校工學院 3 樓資工系會議室 313 室</h3>
     <div id="remaining_time">
       <span class="material-icons">watch_later</span>
-      <h3 id="time_content">2 小時 23 分鐘後開始</h3>
+      <h3 id="time_content">1 小時 43 分鐘後開始</h3>
     </div>
     <div id="banner_btn">
       <a href="#">前往&nbsp;&nbsp;&nbsp;&nbsp;></a>
